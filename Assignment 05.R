f9109ffca1d2c8fa5fe4b3421a8448b78b92a067
Noelle <naerikson@gmail.com>
2019-11-11 15:53:59 -0500

    Changed R Script to say T10YIE.csv

---
+++ Assignment 05.R
@@ -0,0 +1,32 @@
+# Importing the file and measuring how long the import takes
+### Run as a block of text to time #########
+ptm <- proc.time()
+DF <- read.csv("T10YIE.csv")
+CSV_READ_TIME <- (proc.time() - ptm)
+CSV_READ_TIME
+############################################
+
+# Looking at what we got
+class(DF)
+typeof(DF)
+str(DF)
+
+
+## Reading in our csv file using fread() from package data.table 
+# Installing data.table (if required) and loading it into memory
+if (!require("data.table")) install.packages("data.table")
+library("data.table")
+
+#Checking and setting number of cpu threads
+getDTthreads()
+getDTthreads(verbose=TRUE)
+setDTthreads(0)
+getDTthreads()
+
+# Doing a timed read of the same file with fread()
+### Run as a block of text to time #########
+ptm <- proc.time()
+DF <- fread("T10YIE.csv", header="auto", 
+            data.table=FALSE)
+FREAD_READ_TIME <- (proc.time() - ptm)
+FREAD_READ_TIME
